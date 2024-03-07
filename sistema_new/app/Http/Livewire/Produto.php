9858202e116ab9c9847e09467e77ccc89718fcd1
juanlucasarchangelo <jlarchangelo@outlook.com>
2024-03-07 10:12:16 -0300

    Inserção das models e controllers

---
+++ sistema_new/app/Http/Livewire/Produto.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Produto extends Component
+{
+    public function render()
+    {
+        return view('livewire.produto');
+    }
+}
