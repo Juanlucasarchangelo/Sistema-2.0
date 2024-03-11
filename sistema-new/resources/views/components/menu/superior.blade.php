2f1228c419a6c10ced652cbc32220f60baed8edb
juanlucasarchangelo <jlarchangelo@outlook.com>
2024-03-11 16:41:30 -0300

    Inserção e criação dos componentes e painel

--- sistema-new/resources/views/components/menu/superior.blade.php
+++ sistema-new/resources/views/components/menu/superior.blade.php
@@ -4,7 +4,7 @@
             <h2>Dashboard</h2>
             <ul class="breadcrumb padding-0">
                 <li class="breadcrumb-item"><a href="index.html"><i class="zmdi zmdi-home"></i></a></li>
-                <li class="breadcrumb-item active">Dashboard</li>
+                <li class="breadcrumb-item active">Painel</li>
             </ul>
         </div>
         <div class="col-lg-7 col-md-7 col-sm-12">
